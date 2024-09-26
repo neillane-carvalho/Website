4c11783f748235c9ad802bed57097f2e58ad9b8b
neillane-carvalho <neillane.rosario@ucsal.edu.br>
2024-09-26 17:27:58 -0300

    0.0.1

---
+++ website1/script.js
@@ -0,0 +1,7 @@
+document.getElementById('opcao 1').addEventListener('click', function() {
+    document.getElementById('mensagem 1').textContent = 'Nós pensamos iguais!';
+});
+
+document.getElementById('opcao 2').addEventListener('click', function() {
+    document.getElementById('mensagem 2').textContent = 'Eu prefiro a música hihi';
+});
